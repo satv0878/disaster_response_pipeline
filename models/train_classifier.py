bd2d80389d01343fed9ae208ebe290f004808388
Tobias Verleger <tobias.verleger@kalera.com>
2022-07-15 19:41:44 +0200

    updated tokenize load and build modek method

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,63 @@
+import re
 import sys
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sqlalchemy import create_engine
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+from sklearn.model_selection import train_test_split
+
+import nltk
+
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+lemmatizer = WordNetLemmatizer()
 
 
 def load_data(database_filepath):
-    pass
+    '''reads sqlite data from the database_filepath and loads it into an pandas dataframe'''
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table(
+        'drp',
+        con=engine)
+    return df
 
 
 def tokenize(text):
-    pass
+    ''''extract only text remove points and whitespace and to lower'''
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # tokenize
+    words = word_tokenize(text)
+    # remove stopwords
+    words = [w for w in words if w not in stopwords.words("english")]
+    # lemmatize
+
+    clean_tokens = [lemmatizer.lemmatize(word) for word in words]
+    return clean_tokens
 
 
 def build_model():
-    pass
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters = {'vect__min_df': [1, 5],
+                  'tfidf__use_idf': [True, False],
+                  'clf__estimator__n_estimators': [10, 25],
+                  'clf__estimator__min_samples_split': [2, 4]}
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=4, verbose=2)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -26,14 +73,15 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -43,9 +91,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
