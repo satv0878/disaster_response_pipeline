8295505b1003b9a85b4915aecdceae5f5872238d
Tobias Verleger <tobias.verleger@kalera.com>
2022-07-15 19:19:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Disaster Response Pipeline Project
+
+## 1. Installations
+
+| Library    | Version | Link                                                 |
+| ---------- | ------- | ---------------------------------------------------- |
+| Python     | 3.9.0   | https://www.python.org/downloads/release/python-390/ |
+| virtualenv |         |                                                      |
+| SQLAlchemy | 1.4.39  |                                                      |
+| pandas     | 1.4.3   |                                                      |
+| numpy      | 1.23.1  |                                                      |
+
+## 2. Project Motivation
+
+## 3. File Descriptions
+
+## 4. How to Interact with your project
+
+### Active the virtual environment
+
+To active the virtual environment use the follwoing command
+
+```
+.\venv\Scripts\activate
+```
+
+### Deactive the virtual environment
+
+To deactive the virtual environment use the follwoing command
+
+```
+deactive
+```
+
+### Preprocess data
+
+In the data folder raw data for creating the sqllite database can be found. In order to create the database you have to navigate to the folder and execute the process_data.py with follwoing arguments:
+
+- messages_filepath
+- categories_filepath
+- database_filepath
+
+```
+python .\process_data.py "disaster_messages.csv" "disaster_categories.csv" "sqlite:///drp.db"
+```
+
+### Instructions:
+
+1. Run the following commands in the project's root directory to set up your database and model.
+
+   - To run ETL pipeline that cleans data and stores in database
+     `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+   - To run ML pipeline that trains classifier and saves
+     `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+2. Go to `app` directory: `cd app`
+
+3. Run your web app: `python run.py`
+
+4. Click the `PREVIEW` button to open the homepage
---
+++ data/process_data.py
@@ -0,0 +1,74 @@
+
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    reads the data files in to a pandas dataframe
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    dataframe = messages.merge(categories, on='id')
+
+    return dataframe
+
+
+def clean_data(dataframe):
+    '''cleans the dataframe and drops dublicate columns'''
+    categories = dataframe['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda col: col[:-2])
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(
+            lambda col: int(col[-1:]))
+
+    dataframe = dataframe.drop(columns='categories')
+
+    dataframe = pd.concat([dataframe, categories], axis=1)
+
+    dataframe = dataframe.drop_duplicates()
+
+    return dataframe
+
+
+def save_data(dataframe, database_filename):
+    ''' stores the dataframe into and sqlite db '''
+    # database_filename 'sqlite:///drp.db'
+    engine = create_engine(database_filename)
+    dataframe.to_sql('drp', engine, index=False)
+
+
+def main():
+    '''main function which is executed when the pyhton will be executed'''
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
